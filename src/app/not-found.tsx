069c2d427f0e1cb3cb32e0bf42a30c4c484d6f9c
Stanisław Synowiec <stasyn1410@gmail.com>
2023-12-18 16:04:11 +0100

    chore: add primary not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <main className='flex h-full flex-col items-center justify-center text-5xl font-bold'>
+      <h1>404 not found</h1>
+    </main>
+  );
+};
+
+export default NotFound;
