5533db587eaef6ad6e8c61d5c4d3899bcbb58dbf
Stanisław Synowiec <stasyn1410@gmail.com>
2023-12-18 15:01:57 +0100

    fix: gtag types

--- gtag.tsx
+++ gtag.tsx
@@ -1,12 +1,23 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
 
-export const pageview = (url) => {
+export const pageview = (url: string) => {
   window.gtag('config', GA_TRACKING_ID, {
     page_path: url,
   });
 };
 
-export const event = ({ action, category, label, value }) => {
+export const event = ({
+  action,
+  category,
+  label,
+  value,
+}: {
+  action: string;
+  category: string;
+  label: string;
+  value: string;
+}) => {
+  // @ts-ignore
   window.gtag('event', action, {
     event_category: category,
     event_label: label,
