77e9eca5fe30e66117b119799dcbc20dfb7387ec
Stanisław Synowiec <stasyn1410@gmail.com>
2023-12-18 21:41:53 +0100

    style: header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,17 @@
 import { Wrapper } from '@/components/wrapper/wrapper';
 import { ThemeSwitcher } from '@/components/themeSwitcher/themeSwitcher';
+import { Navigation } from '@/components/navigation/navigation';
+import Link from 'next/link';
 
 export const Header = () => {
   return (
-    <header className='flex w-full'>
+    <header>
       <Wrapper>
-        <div>
+        <div className='flex w-screen items-center justify-between'>
+          <div className='flex'>
+            <Link href='/'>Stanisław Synowiec</Link>
+            <Navigation />
+          </div>
           <ThemeSwitcher />
         </div>
       </Wrapper>
