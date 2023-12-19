1f13d1a22ab79400cf288ba9dcfd34c211d7f223
Stanisław Synowiec <stasyn1410@gmail.com>
2023-12-19 19:59:03 +0100

    chore: add primary contact form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,6 @@
 import { Wrapper } from '@/components/wrapper/wrapper';
 import { Metadata } from 'next';
+import { ContactForm } from '@/components/contactForm/contactForm';
 
 export const metadata: Metadata = {
   title: 'Contact with me | ssynowiec.pl',
@@ -9,8 +10,9 @@ const ContactPage = () => {
   return (
     <main className='flex h-full flex-col'>
       <Wrapper>
-        <section className='flex w-full'>
+        <section className='flex w-full flex-col'>
           <h1 className='text-left text-5xl font-bold'>Contact</h1>
+          <ContactForm />
         </section>
       </Wrapper>
     </main>
---
+++ src/components/contactForm/contactForm.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+interface Inputs {
+  name: string;
+  email: string;
+  message: string;
+}
+
+export const ContactForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data: any) => console.log(data);
+
+  return (
+    <form className='flex flex-col py-3' onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor='name'>Name</label>
+      <input type='text' id='name' {...register('name')} />
+      <label htmlFor='email'>Email</label>
+      <input type='email' id='email' {...register('email')} />
+      <label htmlFor='message'>Message</label>
+      <textarea id='message' {...register('message')} />
+      <button type='submit'>Send message</button>
+    </form>
+  );
+};
