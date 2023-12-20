d1ee9671a1d17896bf941182e930359086b0e5ed
Stanisław Synowiec <stasyn1410@gmail.com>
2023-12-20 15:22:48 +0100

    style: add gradient to page title

--- src/components/pageTitle/pageTitle.tsx
+++ src/components/pageTitle/pageTitle.tsx
@@ -5,5 +5,9 @@ interface PageTitleProps {
 }
 
 export const PageTitle = ({ children }: PageTitleProps) => {
-  return <h1 className='text-6xl font-bold'>{children}</h1>;
+  return (
+    <h1 className='bg-gradient-to-br from-[#ec4899] via-[#ef4444] via-30% to-[#eab308] bg-clip-text text-6xl font-bold text-transparent'>
+      {children}
+    </h1>
+  );
 };
