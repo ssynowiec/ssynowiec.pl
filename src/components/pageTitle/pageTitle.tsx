3f42e5947e66de61d24dfd6e01bb2bc9f411fbb3
Stanisław Synowiec <stasyn1410@gmail.com>
2023-12-20 16:15:04 +0100

    chore: add tag variant to page title

--- src/components/pageTitle/pageTitle.tsx
+++ src/components/pageTitle/pageTitle.tsx
@@ -1,13 +1,14 @@
 import { ReactNode } from 'react';
 
 interface PageTitleProps {
+  tag?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'span' | 'p';
   children: ReactNode;
 }
 
-export const PageTitle = ({ children }: PageTitleProps) => {
+export const PageTitle = ({ tag: Tag = 'h1', children }: PageTitleProps) => {
   return (
-    <h1 className='bg-gradient-to-br from-[#ec4899] via-[#ef4444] via-30% to-[#eab308] bg-clip-text text-6xl font-bold text-transparent'>
+    <Tag className='bg-gradient-to-br from-[#ec4899] via-[#ef4444] via-30% to-[#eab308] bg-clip-text text-6xl font-bold text-transparent'>
       {children}
-    </h1>
+    </Tag>
   );
 };
