b56623fd937052c4753db159497b7b243bb342d1
Stanisław Synowiec <stasyn1410@gmail.com>
2023-12-20 13:59:33 +0100

    fix: config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
+import pwa from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: 'build',
 };
 
-const withPwa = require('next-pwa')({
+const withPwa = pwa({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
 });
 
 module.exports = withPwa(nextConfig);
