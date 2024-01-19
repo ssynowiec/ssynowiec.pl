27189d9db2b5337dbde1034053188a21b48c97f0
Stanisław Synowiec <stasyn1410@gmail.com>
2024-01-19 12:51:20 +0100

    add: social links to footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,11 +1,34 @@
 import { Wrapper } from '@/components/wrapper/wrapper';
+import Link from 'next/link';
 
 const currentYear = new Date().getFullYear();
 
 export const Footer = () => {
   return (
     <footer className='flex w-full'>
-      <Wrapper>
+      <Wrapper direction='column'>
+        <div className='self-start text-left'>
+          <ul className='flex'>
+            <li>
+              <Link
+                href='https://github.com/ssynowiec'
+                target='_blank'
+                className='p-2'
+              >
+                Github
+              </Link>
+            </li>
+            <li>
+              <Link
+                href='https://www.linkedin.com/in/ssynowiecpl'
+                target='_blank'
+                className='p-2'
+              >
+                Linkedin
+              </Link>
+            </li>
+          </ul>
+        </div>
         <p className='w-full text-center'>
           &copy; {currentYear} Stanisław Synowiec. All rights reserved.
         </p>
